b21d076006110c9b47637239fa6a66a1ffd0ddab
Jhan delos Santos <jhanleedelossantos@gmail.com>
2021-03-16 22:37:21 +0800

    Added Grasshopper Personalized Message

---
+++ GrasshopperPersonalizedMessage/src/index.js
@@ -0,0 +1,12 @@
+function greet (name, owner) {
+    const own = 'Hello boss';
+    const not = 'Hello guest';
+    if (name === owner){
+       return own;
+    }
+       else {
+         return not;
+       }
+  }
+
+  module.exports = greet;
---
+++ GrasshopperPersonalizedMessage/test/index.js
@@ -0,0 +1,18 @@
+let greet = require('../src/index');
+let assert = require('assert');
+
+describe ('move', function(){
+    it('test', function(){
+        let ans = greet("Daniel", "Daniel");
+        assert.strictEqual(ans, "Hello boss");
+    })
+    it('test', function(){
+        let ans = greet("Greg", "Daniel");
+        assert.strictEqual(ans, "Hello guest");
+    })
+    it('test', function(){
+        let ans = greet("Greg", "Greg");
+        assert.strictEqual(ans, "Hello boss");
+    })
+    
+})
